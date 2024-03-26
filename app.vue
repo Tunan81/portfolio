bacd7d6e17d4170a7c6e0acb5686761ad1464bc2
zy <1840354092@qq.com>
2024-03-26 17:08:10 +0800

    🚦init

--- app.vue
+++ app.vue
@@ -32,22 +32,8 @@
     </q-item>
     <div class="q-pa-md">
       <q-list bordered separator style="max-width: 318px">
-        <q-item v-ripple clickable>
-          <q-item-section>Single line item</q-item-section>
-        </q-item>
-
-        <q-item v-ripple clickable>
-          <q-item-section>
-            <q-item-label>Item with caption</q-item-label>
-            <q-item-label caption> Caption </q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item v-ripple clickable>
-          <q-item-section>
-            <q-item-label overline> OVERLINE </q-item-label>
-            <q-item-label>Item with caption</q-item-label>
-          </q-item-section>
+        <q-item v-ripple clickable v-for="item in postList" :key="item._id">
+          <q-item-section>{{item.title}}</q-item-section>
         </q-item>
       </q-list>
     </div>
@@ -128,9 +114,11 @@
 </template>
 <script setup>
 import {blog_articlesList} from "./api/modules/api.blog_articles";
+const postList = ref([])
 
 blog_articlesList({}).then(res=>{
-  // console.log(res)
+  postList.value = res.data.result || []
+  console.log(res.data.result)
 })
 
 onMounted(()=>{
